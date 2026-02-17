5ccc6f796f2400c14c395fc98be0bccd0a1ae641
M7mdQash <sil.qash@protonmail.com>
2026-02-17 17:44:35 +0300

    small touchup

--- solution.py
+++ solution.py
@@ -6,4 +6,5 @@ subTotal = Item_price * quantity_item
 tax_amount = subTotal * tax_rate
 total_price = subTotal + tax_amount
 print("Item price:","SAR", Item_price, "\nQuantity:", quantity_item, "\nTax rate:", tax_rate * 100, "%")
+print("\n======================\n")
 print("Subtotal:","SAR", subTotal, "\nTax amount:","SAR", tax_amount, "\nTotal Price:","SAR", total_price)
